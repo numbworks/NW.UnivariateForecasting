a1d476c5dd1cc2a74cb1756d9fc65cb71cf340ae
Rubèn <numbworks@gmail.com>
2020-10-04 14:12:52 +0200

    IUnivariateForecaster: moved summary.

--- src/NW.TimeSeriesForecaster/IUnivariateForecaster.cs
+++ src/NW.TimeSeriesForecaster/IUnivariateForecaster.cs
@@ -3,16 +3,15 @@ using System.Collections.Generic;
 
 namespace NW.UnivariateForecasting
 {
+    /// <summary>
+    /// Forecasts the next value for the provided <see cref="SlidingWindow"/> according to Univariate Forecasting.
+    /// <para>Explaination: "[...] univariate refers to an expression, equation, function or polynomial of only one variable [...]
+    /// which consists of observations on only a single characteristic or attribute.".</para>     
+    /// </summary>
     public interface IUnivariateForecaster
     {
 
-        /// <summary>
-        /// Forecasts the next value for the provided <see cref="SlidingWindow"/> according to Univariate Forecasting.
-        /// <para>Explaination: "[...] univariate refers to an expression, equation, function or polynomial of only one variable [...]
-        /// which consists of observations on only a single characteristic or attribute.".</para>     
-        /// </summary>
         Observation Forecast(SlidingWindow objSlidingWindow);
-
         SlidingWindow ForecastAndCombine(SlidingWindow slidingWindow);
         SlidingWindow ForecastAndCombine(SlidingWindow slidingWindow, uint steps);
         List<double> ExtractXActualValues(SlidingWindow slidingWindow);
