6322a9e4add4c1f41c2e0da3a4a01cae885b04f5
Rubèn <numbworks@gmail.com>
2022-12-29 23:42:42 +0100

    #5 SlidingWindowItemManager: removed all the Invoke() statements.

--- src/NW.UnivariateForecasting/SlidingWindows/SlidingWindowItemManager.cs
+++ src/NW.UnivariateForecasting/SlidingWindows/SlidingWindowItemManager.cs
@@ -59,7 +59,7 @@ namespace NW.UnivariateForecasting.SlidingWindows
         {
 
             if (intervalUnit != IntervalUnits.Months)
-                throw new ArgumentException(Validation.MessageCollection.ProvidedIntervalUnitNotSupported.Invoke(intervalUnit.ToString()));
+                throw new ArgumentException(Validation.MessageCollection.ProvidedIntervalUnitNotSupported(intervalUnit.ToString()));
 
             Interval interval = new Interval()
             {
