fa40c72e77a7d94256d57f88a5235f3d1f5e27b5
Rubèn <numbworks@gmail.com>
2023-01-18 19:29:11 +0100

    #8 FilenameFactory, I*, *Tests: added.

---
+++ src/NW.UnivariateForecasting/Filenames/FilenameFactory.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using NW.UnivariateForecasting.Validation;
+
+namespace NW.UnivariateForecasting.Filenames
+{
+    /// <inheritdoc cref="IFilenameFactory"/>
+    public class FilenameFactory : IFilenameFactory
+    {
+
+        #region Fields
+        #endregion
+
+        #region Properties
+
+        public static string DefaultFileNameTemplate { get; } = "{0}_{1}_{2}.{3}";
+        public static string DefaultMainToken { get; } = "unifor";
+        public static string DefaultSessionToken { get; } = "session";
+        public static string DefaultFormatNow { get; } = "yyyyMMddHHmmssfff";
+        public static string DefaultJsonExtension { get; } = "json";
+
+        #endregion
+
+        #region Constructors
+
+        ///<summary>Initializes a <see cref="FilenameFactory"/> instance.</summary>
+        public FilenameFactory() { }
+
+        #endregion
+
+        #region Methods_public
+
+        public string CreateForSessionJson(string folderPath, DateTime now)
+            => ValidateAndCreate(folderPath, DefaultMainToken, DefaultSessionToken, now, DefaultJsonExtension);
+
+        #endregion
+
+        #region Methods_private
+
+        private string ValidateAndCreate(string folderPath, string mainToken, string secondaryToken, DateTime now, string extension)
+        {
+
+            Validator.ValidateStringNullOrWhiteSpace(folderPath, nameof(folderPath));
+
+            string template = DefaultFileNameTemplate;
+            string nowstring = now.ToString(DefaultFormatNow);
+
+            string fileName = string.Format(template, mainToken, secondaryToken, nowstring, extension);
+
+            return Path.Combine(folderPath, fileName);
+
+        }
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 18.01.2023
+*/
---
+++ tests/NW.UnivariateForecasting.UnitTests/Filenames/ObjectMother.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NW.UnivariateForecasting.Filenames;
+
+namespace NW.UnivariateForecasting.UnitTests.Filenames
+{
+    public static class ObjectMother
+    {
+
+        #region Proprietes
+
+        public static string FakeFilePath = @"C:\";
+
+        public static DateTime FakeNow = new DateTime(2021, 05, 01);
+        public static string FakeNowString = FakeNow.ToString(FilenameFactory.DefaultFormatNow);
+
+        public static string Filename_SessionJson
+            = string.Concat(
+                        FakeFilePath,
+                        FilenameFactory.DefaultMainToken,
+                        "_",
+                        FilenameFactory.DefaultSessionToken,
+                        "_",
+                        FakeNowString,
+                        ".",
+                        FilenameFactory.DefaultJsonExtension
+                        );
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 18.01.2023
+*/
