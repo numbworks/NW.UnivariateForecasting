4f77a6a9b9df87a96cbde221176ed013feba52ff
Rubèn <numbworks@gmail.com>
2023-01-18 23:47:04 +0100

    #9 Application namespace: added.

---
+++ src/NW.UnivariateForecastingClient/Application/IApplicationManager.cs
@@ -0,0 +1,16 @@
+﻿namespace NW.UnivariateForecastingClient.Application
+{
+    /// <summary>Represents the CLI application.</summary>
+    public interface IApplicationManager
+    {
+
+        /// <summary>Executes the CLI application according to the given arguments.</summary>
+        int Execute(params string[] args);
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 18.01.2023
+*/
