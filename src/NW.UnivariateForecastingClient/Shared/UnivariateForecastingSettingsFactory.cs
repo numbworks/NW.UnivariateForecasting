edce83b3fa8389a5cfacd7b904a8d51d01caf8eb
Rubèn <numbworks@gmail.com>
2023-01-18 23:25:13 +0100

    #9 UnivariateForecastingSettingsFactory, I*: added.

---
+++ src/NW.UnivariateForecastingClient/Shared/UnivariateForecastingSettingsFactory.cs
@@ -0,0 +1,35 @@
+﻿using NW.UnivariateForecasting;
+
+namespace NW.UnivariateForecastingClient.Shared
+{
+    /// <inheritdoc cref="IUnivariateForecastingSettingsFactory"/>
+    public class UnivariateForecastingSettingsFactory : IUnivariateForecastingSettingsFactory
+    {
+
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructors
+
+        /// <summary>Initializes a <see cref="UnivariateForecastingSettingsFactory"/> instance.</summary>
+        public UnivariateForecastingSettingsFactory() { }
+
+        #endregion
+
+        #region Methods_public
+
+        public UnivariateForecastingSettings Create()
+            => new UnivariateForecastingSettings();
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 18.01.2023
+*/
