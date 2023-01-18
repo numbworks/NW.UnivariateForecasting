69e638f69ed79d6f9af80f2e8786dc756a9cc821
Rubèn <numbworks@gmail.com>
2023-01-18 23:30:28 +0100

    #9 UnivariateForecasterFactory, I*: added.

---
+++ src/NW.UnivariateForecastingClient/Shared/IUnivariateForecasterFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NW.UnivariateForecasting;
+
+namespace NW.UnivariateForecastingClient.Shared
+{
+    /// <summary>A factory for <see cref="TextClassifier"/>.</summary>
+    public interface IUnivariateForecasterFactory
+    {
+
+        /// <summary>Creates an instance of <see cref="TextClassifier"/>.</summary>
+        /// <exception cref="ArgumentNullException"/>
+        UnivariateForecaster Create(UnivariateForecastingSettings settings, UnivariateForecastingComponents components);
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 18.01.2023
+*/
