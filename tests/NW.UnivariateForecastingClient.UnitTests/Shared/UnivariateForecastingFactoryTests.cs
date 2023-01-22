7301671bd6592a1c883dd8b5989adaa3ee4c6eef
Rubèn <numbworks@gmail.com>
2023-01-22 12:43:39 +0100

    #9 UnivariateForecastingFactoryTests: added.

---
+++ tests/NW.UnivariateForecastingClient.UnitTests/Shared/UnivariateForecastingFactoryTests.cs
@@ -0,0 +1,60 @@
+﻿using NW.UnivariateForecasting;
+using NW.UnivariateForecastingClient.Shared;
+using NUnit.Framework;
+
+namespace NW.UnivariateForecastingClient.UnitTests
+{
+    [TestFixture]
+    public class UnivariateForecastingFactoryTests
+    {
+
+        #region Fields
+        #endregion
+
+        #region SetUp
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void UnivariateForecastingSettingsFactory_ShouldCreateAnObjectOfTypeUnivariateForecastingSettingsFactory_WhenInvoked()
+        {
+
+            // Arrange
+            // Act
+            UnivariateForecastingSettingsFactory actual = new UnivariateForecastingSettingsFactory();
+
+            // Assert
+            Assert.IsInstanceOf<UnivariateForecastingSettingsFactory>(actual);
+
+        }
+
+        [Test]
+        public void Create_ShouldCreateAnObjectOfTypeUnivariateForecastingSettings_WhenInvoked()
+        {
+
+            // Arrange
+            // Act
+            UnivariateForecastingSettings actual
+                = new UnivariateForecastingSettingsFactory().Create();
+
+            // Assert
+            Assert.IsInstanceOf<UnivariateForecastingSettings>(actual);
+
+        }
+
+        #endregion
+
+        #region TearDown
+        #endregion
+
+        #region Support_methods
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 22.01.2023
+*/
--- tests/NW.UnivariateForecastingClient.UnitTests/Shared/UnivariateForecastingSettingsFactoryTests.cs
+++ tests/NW.UnivariateForecastingClient.UnitTests/Shared/UnivariateForecastingSettingsFactoryTests.cs
@@ -5,7 +5,7 @@ using NUnit.Framework;
 namespace NW.UnivariateForecastingClient.UnitTests
 {
     [TestFixture]
-    public class UnivariateForecasterFactoryTests
+    public class UnivariateForecastingSettingsFactoryTests
     {
 
         #region Fields
