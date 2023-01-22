80489a6c7af4181c9c09f44d65a6e7216f72c747
Rubèn <numbworks@gmail.com>
2023-01-22 12:39:54 +0100

    #9 UnivariateForecasterFactoryTests: added.

---
+++ tests/NW.UnivariateForecastingClient.UnitTests/Shared/UnivariateForecasterFactoryTests.cs
@@ -0,0 +1,63 @@
+﻿using NW.UnivariateForecasting;
+using NW.UnivariateForecastingClient.Shared;
+using NUnit.Framework;
+
+namespace NW.UnivariateForecastingClient.UnitTests
+{
+    [TestFixture]
+    public class UnivariateForecasterFactoryTests
+    {
+
+        #region Fields
+        #endregion
+
+        #region SetUp
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void UnivariateForecasterFactory_ShouldCreateAnObjectOfTypeUnivariateForecasterFactory_WhenInvoked()
+        {
+
+            // Arrange
+            // Act
+            UnivariateForecasterFactory actual = new UnivariateForecasterFactory();
+
+            // Assert
+            Assert.IsInstanceOf<UnivariateForecasterFactory>(actual);
+
+        }
+
+        [Test]
+        public void Create_ShouldCreateAnObjectOfTypeUnivariateForecaster_WhenInvoked()
+        {
+
+            // Arrange
+            // Act
+            UnivariateForecaster actual 
+                = new UnivariateForecasterFactory().Create(
+                        components: new UnivariateForecastingComponents(), 
+                        settings: new UnivariateForecastingSettings()
+                        );
+
+            // Assert
+            Assert.IsInstanceOf<UnivariateForecaster>(actual);
+
+        }
+
+        #endregion
+
+        #region TearDown
+        #endregion
+
+        #region Support_methods
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 22.01.2023
+*/
