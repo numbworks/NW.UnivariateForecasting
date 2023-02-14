3c55b7e93a6ce1a10712e55bfe4fbf1d187eaac0
Rubèn <numbworks@gmail.com>
2023-02-15 00:34:07 +0100

    #14 SlidingWindows.MessageCollection: removed un-necessary members.

--- src/NW.UnivariateForecasting/SlidingWindows/MessageCollection.cs
+++ src/NW.UnivariateForecasting/SlidingWindows/MessageCollection.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using NW.UnivariateForecasting.Intervals;
 
 namespace NW.UnivariateForecasting.SlidingWindows
 {
@@ -16,18 +15,15 @@ namespace NW.UnivariateForecasting.SlidingWindows
             = (id) => $"The provided {nameof(SlidingWindow.Id)} is: '{id}'.";
         public static Func<string, string> ProvidedObservationNameIs { get; }
             = (observationName) => $"The provided {nameof(SlidingWindow.ObservationName)} is: '{observationName}'.";
-        public static Func<Interval, string> ProvidedIntervalIs { get; }
-            = (interval) => $"The provided {nameof(SlidingWindow.Interval)} is: '{interval}'.";
+
         public static Func<List<SlidingWindowItem>, string> ProvidedItemsCountIs { get; }
             = (items) => $"The provided {nameof(SlidingWindow.Items)} count is: '{items.Count}'.";
-        public static string CreatingIntervalOutOfFollowingArguments { get; }
-            = $"Creating a {typeof(Interval).Name} out of the provided arguments...";
+
         public static Func<uint, string> ProvidedStepsAre { get; }
             = (steps) => $"The provided steps are: '{steps}'.";
         public static Func<List<double>, string> ProvidedValuesAre { get; }
             = (values) => $"The provided steps are: '{values.Count}'.";
-        public static Func<IntervalUnits, string> ProvidedIntervalUnitsIs { get; }
-            = (intervalUnits) => $"The provided {typeof(IntervalUnits).Name} is: '{intervalUnits}'.";
+
         public static Func<SlidingWindow, string> FollowingSlidingWindowHasBeenCreated { get; }
             = (slidingWindow) => $"The following {typeof(SlidingWindow).Name} has been created: '{slidingWindow.ToString(true)}'.";
 
@@ -38,5 +34,5 @@ namespace NW.UnivariateForecasting.SlidingWindows
 
 /*
     Author: numbworks@gmail.com
-    Last Update: 12.11.2022
+    Last Update: 14.02.2023
 */
