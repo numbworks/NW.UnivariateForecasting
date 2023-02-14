470e82d1380143069ecbf5cbd42e3271c4a595fe
Rubèn <numbworks@gmail.com>
2023-02-15 00:12:10 +0100

    #14 SlidingWindowItem: removed Interval.

--- src/NW.UnivariateForecasting/SlidingWindows/SlidingWindowItem.cs
+++ src/NW.UnivariateForecasting/SlidingWindows/SlidingWindowItem.cs
@@ -1,6 +1,4 @@
-﻿using NW.UnivariateForecasting.Intervals;
-
-namespace NW.UnivariateForecasting.SlidingWindows
+﻿namespace NW.UnivariateForecasting.SlidingWindows
 {
     /// <summary>An item within a <see cref="SlidingWindow"/>.</summary>
     public class SlidingWindowItem
@@ -12,7 +10,6 @@ namespace NW.UnivariateForecasting.SlidingWindows
         #region Properties
 
         public uint Id { get; set; }
-        public Interval Interval { get; set; }
         public double X_Actual { get; set; }
         public double? Y_Forecasted { get; set; }
 
@@ -30,14 +27,13 @@ namespace NW.UnivariateForecasting.SlidingWindows
         public override string ToString()
         {
 
-            // [ Id: '0', Interval: 'null', X_Actual: '0', Y_Forecasted: 'null' ]
-            // [ Id: '1', Interval: '20190131:20190228:20190331', X_Actual: '58,5', Y_Forecasted: '615,26' ]
+            // [ Id: '0', X_Actual: '0', Y_Forecasted: 'null' ]
+            // [ Id: '1', X_Actual: '58,5', Y_Forecasted: '615,26' ]
 
             string content
                 = string.Join(
                     ", ",
                     $"{nameof(Id)}: '{Id}'",
-                    $"{nameof(Interval)}: '{Interval?.ToString(true) ?? "null"}'",
                     $"{nameof(X_Actual)}: '{X_Actual}'",
                     $"{nameof(Y_Forecasted)}: '{Y_Forecasted?.ToString() ?? "null"}'"
                     );
@@ -53,5 +49,5 @@ namespace NW.UnivariateForecasting.SlidingWindows
 
 /*
     Author: numbworks@gmail.com
-    Last Update: 11.10.2021
+    Last Update: 14.02.2023
 */
