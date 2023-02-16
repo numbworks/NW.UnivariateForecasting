2a58dc582724342a56263b90ad6f15807d61831f
Rubèn <numbworks@gmail.com>
2023-02-16 23:34:49 +0100

    #15 SlidingWindowTests: fixed accordingly.

--- tests/NW.UnivariateForecasting.UnitTests/SlidingWindows/SlidingWindowTests.cs
+++ tests/NW.UnivariateForecasting.UnitTests/SlidingWindows/SlidingWindowTests.cs
@@ -1,5 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
 using NW.UnivariateForecasting.SlidingWindows;
+using NUnit.Framework;
 
 namespace NW.UnivariateForecasting.UnitTests.SlidingWindows
 {
@@ -9,6 +11,17 @@ namespace NW.UnivariateForecasting.UnitTests.SlidingWindows
 
         #region Fields
 
+        private static TestCaseData[] slidingWindowExceptionTestCases =
+        {
+
+            new TestCaseData(
+                new TestDelegate(
+                    () => new SlidingWindow(items: null)),
+                typeof(ArgumentNullException),
+                new ArgumentNullException("items").Message
+                ).SetArgDisplayNames($"{nameof(slidingWindowExceptionTestCases)}_01")
+
+        };
         private static TestCaseData[] toStringTestCases =
         {
 
@@ -19,13 +32,18 @@ namespace NW.UnivariateForecasting.UnitTests.SlidingWindows
                 ).SetArgDisplayNames($"{nameof(toStringTestCases)}_01")
 
         };
-        
+
         #endregion
 
         #region SetUp
         #endregion
 
         #region Tests
+
+        [TestCaseSource(nameof(slidingWindowExceptionTestCases))]
+        public void SlidingWindow_ShouldThrowACertainException_WhenUnproperArguments
+            (TestDelegate del, Type expectedType, string expectedMessage)
+                => Utilities.ObjectMother.Method_ShouldThrowACertainException_WhenUnproperArguments(del, expectedType, expectedMessage);
 
         [TestCaseSource(nameof(toStringTestCases))]
         public void ToString_ShouldReturnExpectedString_WhenInvoked
@@ -40,6 +58,20 @@ namespace NW.UnivariateForecasting.UnitTests.SlidingWindows
             // Assert
             Assert.AreEqual(expected1, actual1);
             Assert.AreEqual(expected2, actual2);
+
+        }
+
+        [Test]
+        public void SlidingWindow_ShouldCreateAnObjectOfThisType_WhenInvoked()
+        {
+
+            // Arrange
+            // Act
+            SlidingWindow actual = new SlidingWindow(items: ObjectMother.SlidingWindow01_Items);
+
+            // Assert
+            Assert.IsInstanceOf<SlidingWindow>(actual);
+            Assert.IsInstanceOf<List<SlidingWindowItem>>(actual.Items);
 
         }
 
