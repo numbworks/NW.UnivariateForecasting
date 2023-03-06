13024825bef1123948d8df3c9bf83100badc804a
Rubèn <numbworks@gmail.com>
2023-03-06 22:28:44 +0100

    #18 SlidingWindows.MessageCollection: fixed ProvidedItemsCountIs.

--- src/NW.UnivariateForecasting/SlidingWindows/MessageCollection.cs
+++ src/NW.UnivariateForecasting/SlidingWindows/MessageCollection.cs
@@ -11,8 +11,6 @@ namespace NW.UnivariateForecasting.SlidingWindows
 
         public static string CreatingSlidingWindowOutOfFollowingArguments { get; }
             = $"Creating a {typeof(SlidingWindow).Name} out of the provided arguments...";
-        public static Func<List<SlidingWindowItem>, string> ProvidedItemsCountIs { get; }
-            = (items) => $"The provided {nameof(SlidingWindow.Items)} count is: '{items.Count}'.";
         public static Func<List<double>, string> ProvidedValuesAre { get; }
             = (values) => $"The provided steps are: '{values.Count}'.";
         public static Func<SlidingWindow, string> FollowingSlidingWindowHasBeenCreated { get; }
@@ -25,5 +23,5 @@ namespace NW.UnivariateForecasting.SlidingWindows
 
 /*
     Author: numbworks@gmail.com
-    Last Update: 16.02.2023
+    Last Update: 06.03.2023
 */
